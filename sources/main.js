e001e30c1f66392f55922bfc5d65135882d174c8
conao3 <conao3@gmail.com>
2019-01-10 18:32:08 +0900

    add main.js

---
+++ sources/main.js
@@ -0,0 +1,20 @@
+$(".dropdown-button").dropdown();
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+});
+
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+});
+
+$(document).ready(function(){
+    $('.modal').modal();
+});
+
+$(document).ready(function(){
+    $('.tabs').tabs();
+});
+
+$(document).ready(function(){
+    $('.tooltipped').tooltip();
+});
